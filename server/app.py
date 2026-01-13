c2bc6ac0f14ae90e1832b8962e807371c594fea8
Avery Morrison <morria4@rpi.edu>
2026-01-13 14:52:44 -0500

    Configure Flask to serve frontend json files

--- server/app.py
+++ server/app.py
@@ -1,15 +1,19 @@
 import os
-from flask import Flask, jsonify
+from flask import Flask, jsonify, send_from_directory
 from dotenv import load_dotenv
 from models import db, Song
 
 load_dotenv()  # reads .env if present
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="static", static_url_path="")
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ["DATABASE_URL"]
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db.init_app(app)
+
+@app.get("/")
+def index():
+    return send_from_directory(app.static_folder, "index.html")
 
 @app.get("/api/health")
 def health():
